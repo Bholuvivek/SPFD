3ff30698d5c0c35ffac0f4d3a7e3c5844665a124
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-01-30 15:16:39 +0530

    Add files via upload

---
+++ Basic_of_JS/Forloop.js
@@ -0,0 +1,11 @@
+for(let i = 1; i <= 10; i++){
+    // console.log(i)
+}
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+}
