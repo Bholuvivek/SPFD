223bd18e7341fb58b88a557ff8343cf3cf660805
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-06 10:46:22 +0000

    Commit

---
+++ Promises/promiseThree.js
@@ -0,0 +1,26 @@
+// In this File I am foing Chaining Concept
+
+const promise = new Promise(function (resolve,  reject) {
+    setTimeout(() => {
+        console.log('Async Consumed');
+        let data = true;
+        if(!true){
+            resolve({MyName: 'Vivek Singh', MyInsta:'@thevivekbholu'})
+        }
+        else{
+            reject('It have Some Error')
+        }
+    }, 1000);
+})
+
+
+// Calling then and also Using Chainig 
+promise.then((user)=>{
+    console.log(user);
+    return user.MyInsta
+}).then(function(userInsta){ // This is chaining if i store then value in any variable then it give the error 
+    // so better to do chaining
+    console.log(userInsta);
+}).catch(function (err) {
+    console.log(err);   
+})
