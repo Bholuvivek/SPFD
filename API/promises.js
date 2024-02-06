728d4c1db8d4970749571a543d57b6a41d8e59b7
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-06 09:56:35 +0000

    commit

--- API/promises.js
+++ API/promises.js
@@ -1,5 +1,18 @@
-const promise = new Promise( function(resolve, reject){
+// This is way to create the Promises
+const promises = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log('Async Task 1');
-    },2000)
+        resolve(); // This is Method wich is connect to the Then
+    },1000)
 })
+// now I am Connecting the 'then()'
+promises.then(function(){
+    console.log('1 Consumed!');
+})
+promises.catch(function name(params) {
+    console.log(params);
+    
+})
+// promises.finally(function (){
+//     console.log('Finally');
+// })
