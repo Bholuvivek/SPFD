50bac51d221bcbdd951d45e2128e9cb267372a71
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-08 05:03:29 +0000

    commit

--- Promises/promisFive.js
+++ Promises/promisFive.js
@@ -3,7 +3,7 @@
 const promise = new Promise(function(resolve, reject){
     setTimeout(() => {
         console.log('Async Consumed');
-        let data = true; // it give error if i keep false then give the resulty
+        let data = false; // it give error if i keep false then give the resulty
         if(!true){
             resolve({MyName: 'Vivek Singh', MyInsta:'@thevivekbholu',MyTwitter:'@Bholuvivek'})
         }
@@ -12,3 +12,19 @@ const promise = new Promise(function(resolve, reject){
         }
     }, 1000);
 })
+async function consumPromis(){
+    try {
+        const response = await promise
+    } catch (error) {
+     console.log(error);   
+    }
+}
+async function consumePromise(){
+    try {
+        const response = await promise
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumPromis()
