592803dc1f64b431c4748ad45c42c9d641a604f2
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-17 16:58:11 +0530

    Commit

---
+++ React/Day1/hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 class= "welcome"> Welcome to Day 1 Let's Start React </h2>   
+    </div>
+  );
+}
+
+export default App;
