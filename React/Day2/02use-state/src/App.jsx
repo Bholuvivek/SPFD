bae273b1154d99fbffd4f39c11af405a807900af
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-23 15:53:20 +0000

    commit

--- React/Day2/02use-state/src/App.jsx
+++ React/Day2/02use-state/src/App.jsx
@@ -4,35 +4,36 @@ import './App.css'
 
 function App() {
 
-  const [counter, setCounter]  = useState(15)
+   const [count, setCounter]  = useState(0)
 
-  //let counter = 15
 
-  const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-    
+  const addCount = () =>{
+    setCounter(count+1)
   }
+  
+  
 
-  const removeValue = () => {
-    setCounter(counter - 1)
+  const removeCount = () => {
+    if(count>0){
+    setCounter(count - 1)
+  }
+  else{
+   const message= document.querySelector('.erro')
+   message.innerHTML = "Sorry The Counter is  Zero"
+  }
   }
   
   return (
     <>
-      <h1>Counter </h1>
-      <h2>Counter value: {counter}</h2>
+    <h1>Let's Create Plus and Minus </h1>
 
-      <button
-      onClick={addValue}
-      >Add value {counter}</button> 
-      <br />
-      <button
-      onClick={removeValue}
-      >remove value {counter}</button>
-      <p>footer: {counter}</p>
+    <h3 className='erro'></h3>
+    <h2>Counter Value is :{count}</h2>
+    <button onClick={addCount} >Click Here To Increase VAlue</button><br /> <br />
+
+    <button onClick={removeCount}>Click Here to Decrese Counter </button>
     </>
-  )
+    )
 }
 
 export default App
