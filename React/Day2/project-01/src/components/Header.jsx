b6999e5b1b2a9a31c5669fac6d56697fa435b8e2
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-24 15:54:23 +0000

    commit

--- React/Day2/project-01/src/components/Header.jsx
+++ React/Day2/project-01/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Header.css'
-import logo from '../assets/log.jpg'
+import logo from '../assets/logo.jpg'
 const Header = () => {
   return (
     <>
     <div className="header">
     <div className="my-logo">
-        <img src={logo} alt="logo" style={{width:'80px'}}  />
+        <img src={logo} alt="logo" style={{width:'50px'}}  />
     </div>
     <div className="nav-item">
         <ul>
@@ -21,5 +21,3 @@ const Header = () => {
     </>
   )
 }
-
-export default Header
