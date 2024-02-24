f1a2baaa934f874aa23a0c11c2f0838d5ab4f24d
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-24 03:02:18 +0000

    commit

--- React/Day3/01integrate-telwind/src/App.jsx
+++ React/Day3/01integrate-telwind/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-
 import './App.css'
 
 function App() {
@@ -8,7 +7,10 @@ function App() {
 
   return (
     <>
-      <h1 className='heading1'>Welcome to Day 3</h1>
+    <div className='heading1'>
+    <h1 >Welcome to Day 3</h1>
+    </div>
+      
     </>
   )
 }
