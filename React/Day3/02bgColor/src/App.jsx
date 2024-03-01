b4975d5f1fc44716b19da288b2ffd06aad7db66f
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-03-01 18:55:42 +0000

    commit

--- React/Day3/02bgColor/src/App.jsx
+++ React/Day3/02bgColor/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-
+import './App.css'
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [colr, setColor] = useState("olive")
 
   return (
     <>
-     <div className="bgColor-" style={{backgroundColor: color, height:'100vh'}}>
-     <h2>Background Changer App</h2>
+     <div className="bgColor-div" style={{backgroundColor: colr, height:'100vh'}}>
+        <div className="btn">
+          <button onClick={()=> setColor('red')} style={{backgroundColor:'red'}}>Red</button>
+          <button onClick={()=> setColor('blue')} style={{backgroundColor:'blue'}}>Blue</button>
+          <button onClick={()=> setColor('yellow')} style={{backgroundColor:'yellow'}}>Yellow</button>
+          <button onClick={()=> setColor('#202020')} style={{backgroundColor:'#202020', color:'white'}}>Dark</button>
+          <button onClick={()=> setColor('pink')} style={{backgroundColor:'pink'}}>Pink</button>
+          <button onClick={()=> setColor('green')} style={{backgroundColor:'green'}}>Green</button>
+          <button onClick={()=> setColor('greenyellow')} style={{backgroundColor:'greenyellow'}}>GreenYellow</button>
+          <button onClick={()=> setColor('#FFF')}style={{backgroundColor:'#FFFFFF'}}>White</button>
+          <button onClick={()=> setColor('#a52a2a')} style={{backgroundColor:'#a52a2a'}}>Brown</button>
+          <button onClick={()=> setColor('#00ffff')} style={{backgroundColor:'#00ffff'}}>Cyan</button>
+          <button onClick={()=> setColor('#800080')} style={{backgroundColor:'#800080'}}>Purple</button>
+
+        </div>
+  
      </div>
     </>
   )
